004cea4adb9924511f708fe7498b345ae5b1b8fa
Seymur Kafkas <seymurkafkas@gmail.com>
2021-01-18 00:13:57 +0300

    Added image projection

--- image_processor/src/image_processor.cpp
+++ image_processor/src/image_processor.cpp
@@ -3,6 +3,50 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <math.h>
+
+cv::Mat maskImage(cv::Mat image)
+{
+    cv::Mat maskedImage;
+    cv::Mat mask = cv::Mat::zeros(image.size(), image.type());
+    cv::Point polygonBoundaries[4] = {
+        cv::Point(30, 410),
+        cv::Point(150, 245),
+        cv::Point(480, 245),
+        cv::Point(610, 410)};
+
+    // Polygon mask
+    cv::fillConvexPoly(mask, polygonBoundaries, 4, cv::Scalar(255, 255, 255));
+    // Multiply the edges image and the mask to get the output
+    cv::bitwise_and(image, mask, maskedImage);
+
+    return maskedImage;
+}
+
+cv::Mat projectImage(const cv::Mat &image)
+
+{
+    // todo do some checks on input.
+
+    cv::Point2f source_points[4];
+    cv::Point2f dest_points[4];
+
+    source_points[0] = cv::Point2f(50, 420);
+    source_points[1] = cv::Point2f(135, 350);
+    source_points[2] = cv::Point2f(490, 350);
+    source_points[3] = cv::Point2f(575, 420);
+
+    dest_points[0] = cv::Point2f(0, 480);
+    dest_points[1] = cv::Point2f(0, 245);
+    dest_points[2] = cv::Point2f(640, 245);
+    dest_points[3] = cv::Point2f(640, 480);
+
+    cv::Mat dst;
+    cv::Mat transformMatrix = cv::getPerspectiveTransform(source_points, dest_points);
+    cv::warpPerspective(image, dst, transformMatrix, image.size());
+
+    return dst;
+}
 
 void rawImageCallback(const sensor_msgs::ImageConstPtr &msg)
 {
@@ -16,7 +60,10 @@ void rawImageCallback(const sensor_msgs::ImageConstPtr &msg)
         cv::GaussianBlur(grayImageMatrix, blurredImage, cv::Size(5, 5), 1);
         cv::Mat lineImage;
         cv::Canny(blurredImage, lineImage, 100, 150, 5, true);
-        cv::imshow("view", lineImage);
+        cv::Mat maskedImage = maskImage(cameraImage);
+
+        cv::Mat projectedImage = projectImage(cameraImage);
+        cv::imshow("view", cameraImage);
         cv::waitKey(30);
     }
 
