bd91dc374b53357bed8d587bb9754ee21627811b
Seymur Kafkas <seymurkafkas@gmail.com>
2021-01-17 19:38:55 +0300

    Added noise filtering, edge detection

--- image_processor/src/image_processor.cpp
+++ image_processor/src/image_processor.cpp
@@ -1,15 +1,25 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>    
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
 
 void rawImageCallback(const sensor_msgs::ImageConstPtr &msg)
 {
     try
     {
-        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+        cv::Mat cameraImage = cv_bridge::toCvShare(msg, "bgr8")->image;
+        cv::Mat grayImageMatrix;
+
+        cv::cvtColor(cameraImage, grayImageMatrix, cv::COLOR_BGR2GRAY);
+        cv::Mat blurredImage;
+        cv::GaussianBlur(grayImageMatrix, blurredImage, cv::Size(5, 5), 1);
+        cv::Mat lineImage;
+        cv::Canny(blurredImage, lineImage, 100, 150, 5, true);
+        cv::imshow("view", lineImage);
         cv::waitKey(30);
     }
+
     catch (cv_bridge::Exception &e)
     {
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
@@ -21,7 +31,6 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "image_processor");
     ros::NodeHandle nh;
     cv::namedWindow("Turtlebot");
-    std::cout << "Heelo";
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("camera/rgb/image_raw", 1, rawImageCallback);
     ros::spin();
